25be25c9217ec6a8a06659dc8b8125ee832c5063
higoka <nico.rickenbach@gmail.com>
2020-01-11 04:13:20 +0100

    initial commit

---
+++ app/js/app.js
@@ -0,0 +1,54 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js')
+    .then((reg) => {
+      reg.addEventListener('updatefound', () => {
+        const worker = reg.installing
+        worker.addEventListener('statechange', () => {
+          switch (worker.state) {
+            case 'installed':
+              if (navigator.serviceWorker.controller) {
+                document.querySelector('.popup').style.display = 'block'
+              }
+              break;
+          }
+        })
+      })
+    })
+}
+
+const result = document.querySelector('.result')
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  if (!form.carb.value && !form.carb.fat) {
+    return
+  }
+
+  const carb1 = `<li>${Math.floor((100 / form.carb.value) * 10)}g KH</li>`
+  const fat1  = `<li>${Math.floor((100 / form.fat.value) * 10)}g Fett</li>`
+
+  const carb2 = `<li>${Math.floor((100 / form.carb.value) * form.amount.value * 10)}g KH</li>`
+  const fat2  = `<li>${Math.floor((100 / form.fat.value) * form.amount.value * 10)}g Fett</li>`
+
+  const amount = `
+    <div>
+      <p>${form.amount.value} Wert</p>
+      <ul>
+        ${form.carb.value ? carb2 : ''}
+        ${form.fat.value ? fat2 : ''}
+      </ul>
+    </div>`
+
+  result.innerHTML = `
+    <div>
+      <p>1 Wert</p>
+      <ul>
+        ${form.carb.value ? carb1 : ''}
+        ${form.fat.value ? fat1 : ''}
+      </ul>
+    </div>
+    ${form.amount.value ? amount : ''}
+  `
+})
---
+++ app/sw.js
@@ -0,0 +1,39 @@
+const cacheName = 'v1.0.0'
+const cacheFiles = [
+  '/',
+  'manifest.json',
+  'index.html',
+  'css/app.css',
+  'js/app.js',
+  'img/icon.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
+  'https://fonts.googleapis.com/css?family=Quicksand:400,700&display=swap',
+  'https://fonts.gstatic.com/s/quicksand/v19/6xKtdSZaM9iE8KbpRA_hK1QNYuDyPw.woff2',
+  // 'https://fonts.gstatic.com/s/quicksand/v19/6xKtdSZaM9iE8KbpRA_hJVQNYuDyP7bh.woff2',
+  // 'https://fonts.gstatic.com/s/quicksand/v19/6xKtdSZaM9iE8KbpRA_hJFQNYuDyP7bh.woff2',
+]
+
+this.addEventListener('install', (e) => {
+  console.log('install')
+  e.waitUntil(
+    caches.open(cacheName)
+      .then((cache) => cache.addAll(cacheFiles))
+  )
+})
+
+this.addEventListener('activate', (e) => {
+  console.log('activate')
+  e.waitUntil(
+    caches.keys()
+      .then((cacheNames) => Promise.all(cacheNames.filter((cache) => cache !== cacheName).map((cache) => caches.delete(cache))))
+  )
+})
+
+this.addEventListener('fetch', (e) => {
+  console.log('fetch')
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request)
+    })
+  )
+})
