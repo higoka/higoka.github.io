769f18c41c0ab88a00afd395937ad78ea5d868fa
higoka <nico.rickenbach@gmail.com>
2020-01-11 04:19:38 +0100

    fix

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js', { scope: '/app/' })
+  navigator.serviceWorker.register('sw.js')
     .then((reg) => {
       reg.addEventListener('updatefound', () => {
         const worker = reg.installing
